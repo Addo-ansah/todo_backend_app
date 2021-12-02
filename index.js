6e1440dce7607c7e34fd00815473283de2d8a4de
Addo-ansah <adrianamargitaddoansah@gmail.com>
2021-12-02 13:33:31 +0000

    commit to master

---
+++ index.js
@@ -0,0 +1,32 @@
+//import the express package
+import express from "express";
+import mongoose from "mongoose";
+
+import dotenv from "dotenv";
+
+dotenv.config();
+
+// create an instance of express
+const app = express();
+app.use(express.json());
+
+//use this to port for our server
+const PORT = 5000;
+// create a route to handle GET request on the index route 
+app.get("/", (req, res) => {
+    res.send("Hello World");
+});
+
+mongoose.connect(process.env.MONGO_DB_CONSTRING, (error) => {
+    if (error) {
+        return console.log("Failed to connect to MongoDB");
+    }
+
+    else {
+        console.log(`MongoDB is connected to the ${PORT}`);
+    }
+});
+
+//you need to listen to the server if it is working
+// on the specified PORT
+app.listen(PORT, () => console.log(`Server is up and runing  PORT: ${PORT}`));
